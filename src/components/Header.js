07ea30015ee130370c8baf23228d4ff74e7dab07
James Dobbs <dobbs282@gmail.com>
2021-09-16 17:19:28 -0400

    header text with style added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,55 @@
 import React from "react";
 
+const styles = {
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    margin: "0 2rem",
+    backgroundColor: "rgba(248, 248, 248, 0.56)",
+  },
+  mainNav: {
+    display: "inline-flex",
+    alignContent: "center",
+    justifyContent: "flex-end",
+  },
+  liTags: {
+    display: "inline",
+    listStyle: "none",
+    marginRight: "2rem",
+    padding: "7px",
+    fontSize: "1.5rem",
+    borderRight: "3px solid #ccc",
+  },
+};
+
 export default function Header() {
   return (
-    <header id="main-header">
+    <header style={styles.header}>
       <h1>James Dobbs</h1>
-      <nav id="main-nav">
+      <nav style={styles.mainNav}>
         <ul>
-          <li>
-            <a href="#about-me">About Me</a>
+          <li style={styles.liTags}>
+            <a
+              style={{ textDecoration: "none", color: "black" }}
+              href="#about-me"
+            >
+              About Me
+            </a>
           </li>
-          <li>
-            <a href="#work">Work</a>
+          <li style={styles.liTags}>
+            <a style={{ textDecoration: "none", color: "black" }} href="#work">
+              Work
+            </a>
           </li>
-          <li>
-            <a href="#main-footer">Contact</a>
+          <li style={styles.liTags}>
+            <a
+              style={{ textDecoration: "none", color: "black" }}
+              href="#main-footer"
+            >
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
